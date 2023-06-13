c99d7974b008cb03f7454d9977a55f05c685b9ad
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2023-06-13 23:16:54 +0300

    Do not launch browser

--- Entities/CourseTeacher.cs
+++ Entities/CourseTeacher.cs
@@ -4,6 +4,7 @@ public class CourseTeacher
 {
     public int TeacherId { get; set; }
     public Teacher Teacher { get; set; }
+
     public int CourseId { get; set; }
     public Course Course { get; set; }
 }
--- Entities/Exam.cs
+++ Entities/Exam.cs
@@ -7,18 +7,10 @@ public class Exam
 {
     public int ExamId { get; set; }
 
-    [Required]
-    public string Title { get; set; }
+    [Required] public string Title { get; set; }
 
     public DateTime ExamDate { get; set; }
 
-    // Assuming each exam is associated with a specific course
     public int CourseId { get; set; }
-    public Course Course { get; set; }
-
-    // If an exam has multiple questions, you might want to represent that
-    // public ICollection<Question> Questions { get; set; }
-
-    // If you're storing exam grades, you might want a navigation property to those
-    // public ICollection<ExamGrade> ExamGrades { get; set; }
+    public Course? Course { get; set; }
 }
